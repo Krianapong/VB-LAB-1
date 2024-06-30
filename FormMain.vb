098bac25cd061e2be904c7c6702f0cb7530cdfbe
Krisanapong Jaruenjanporn <150851052+Krianapong@users.noreply.github.com>
2024-06-30 23:39:28 +0700

    pday

--- FormMain.vb
+++ FormMain.vb
@@ -1,3 +1,14 @@
 ﻿Public Class FormMain
-
+    Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim formHome = New FormHome()
+        switchForm(formHome)
+    End Sub
+    Private Sub switchForm(form As Form)
+        pnlMain.Controls.Clear()
+        form.TopLevel = False
+        form.Dock = DockStyle.Fill
+        pnlMain.Controls.Add(form)
+        form.BringToFront()
+        form.Show()
+    End Sub
 End Class
